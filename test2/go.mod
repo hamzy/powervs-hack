54eb071d946a9739f7217f143e432bd1f2f43a60
Mark Hamzy <hamzy@us.ibm.com>
2022-02-23 08:18:15 -0600

    Add test2 program

---
+++ test2/go.mod
@@ -0,0 +1,27 @@
+module example/user/test2
+
+go 1.17
+
+require (
+	github.com/IBM/go-sdk-core/v5 v5.9.2
+	github.com/IBM/networking-go-sdk v0.26.0
+	github.com/IBM/platform-services-go-sdk v0.22.8
+	github.com/IBM/vpc-go-sdk v1.0.1
+	github.com/pkg/errors v0.9.1
+)
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef // indirect
+	github.com/go-openapi/errors v0.19.8 // indirect
+	github.com/go-openapi/strfmt v0.21.1 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mitchellh/mapstructure v1.3.3 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	go.mongodb.org/mongo-driver v1.7.5 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
+)
---
+++ test2/test2.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+	"github.com/IBM/go-sdk-core/v5/core"
+	"github.com/IBM/networking-go-sdk/zonesv1"
+	"github.com/IBM/platform-services-go-sdk/resourcecontrollerv2"
+	"github.com/IBM/platform-services-go-sdk/resourcemanagerv2"
+	"github.com/IBM/vpc-go-sdk/vpcv1"
+	"github.com/pkg/errors"
+)
+
+func prettyPrint(i interface{}) string {
+    s, _ := json.MarshalIndent(i, "", "\t")
+    return string(s)
+}
+
+// Client makes calls to the PowerVS API.
+type Client struct {
+	ApiKey         string
+	managementAPI *resourcemanagerv2.ResourceManagerV2
+	controllerAPI *resourcecontrollerv2.ResourceControllerV2
+	vpcAPI        *vpcv1.VpcV1
+}
+
+// cisServiceID is the Cloud Internet Services' catalog service ID.
+const cisServiceID = "75874a60-cb12-11e7-948e-37ac098eb1b9"
+
+// DNSZoneResponse represents a DNS zone response.
+type DNSZoneResponse struct {
+	// Name is the domain name of the zone.
+	Name string
+
+	// ID is the zone's ID.
+	ID string
+
+	// CISInstanceCRN is the IBM Cloud Resource Name for the CIS instance where
+	// the DNS zone is managed.
+	CISInstanceCRN string
+
+	// CISInstanceName is the display name of the CIS instance where the DNS zone
+	// is managed.
+	CISInstanceName string
+
+	// ResourceGroupID is the resource group ID of the CIS instance.
+	ResourceGroupID string
+}
+
+// NewClient initializes a client with a session.
+func NewClient() (*Client, error) {
+	apiKey := os.Getenv("IC_API_KEY")
+//	log.Printf("NewClient: apiKey = %v", apiKey)
+
+	client := &Client{
+		ApiKey: apiKey,
+	}
+
+	if err := client.loadSDKServices(); err != nil {
+		return nil, errors.Wrap(err, "failed to load IBM SDK services")
+	}
+
+	return client, nil
+}
+
+func (c *Client) loadSDKServices() error {
+	servicesToLoad := []func() error{
+		c.loadResourceManagementAPI,
+		c.loadResourceControllerAPI,
+		c.loadVPCV1API,
+	}
+
+	// Call all the load functions.
+	for _, fn := range servicesToLoad {
+		if err := fn(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// GetDNSZoneIDByName gets the CIS zone ID from its domain name.
+func (c *Client) GetDNSZoneIDByName(ctx context.Context, name string) (string, error) {
+
+	zones, err := c.GetDNSZones(ctx)
+	log.Printf("GetDNSZoneIDByName: %v %v", zones, err)
+	if err != nil {
+		return "", err
+	}
+
+	for _, z := range zones {
+		if z.Name == name {
+			return z.ID, nil
+		}
+	}
+
+	return "", fmt.Errorf("DNS zone %q not found", name)
+}
+
+// GetDNSZones returns all of the active DNS zones managed by CIS.
+func (c *Client) GetDNSZones(ctx context.Context) ([]DNSZoneResponse, error) {
+	_, cancel := context.WithTimeout(ctx, 1*time.Minute)
+	defer cancel()
+
+	options := c.controllerAPI.NewListResourceInstancesOptions()
+	options.SetResourceID(cisServiceID)
+
+	log.Printf("GetDNSZones: before c = %s", prettyPrint(c))
+	listResourceInstancesResponse, _, err := c.controllerAPI.ListResourceInstances(options)
+	if err != nil {
+		log.Printf("GetDNSZones: c.controllerAPI.ListResourceInstances: %v %v", listResourceInstancesResponse, err)
+		return nil, errors.Wrap(err, "failed to get cis instance")
+	}
+	log.Printf("GetDNSZones: after c = %s", prettyPrint(c))
+
+	var allZones []DNSZoneResponse
+	for _, instance := range listResourceInstancesResponse.Resources {
+		crnstr := instance.CRN
+		authenticator := &core.IamAuthenticator{
+			ApiKey: c.ApiKey,
+		}
+		zonesService, err := zonesv1.NewZonesV1(&zonesv1.ZonesV1Options{
+			Authenticator: authenticator,
+			Crn:           crnstr,
+		})
+		if err != nil {
+			log.Printf("GetDNSZones: zonesv1.NewZonesV1: %v %v", zonesService, err)
+			return nil, errors.Wrap(err, "failed to list DNS zones")
+		}
+
+		options := zonesService.NewListZonesOptions()
+		listZonesResponse, _, err := zonesService.ListZones(options)
+
+		if listZonesResponse == nil {
+			return nil, err
+		}
+
+		for _, zone := range listZonesResponse.Result {
+			if *zone.Status == "active" {
+				zoneStruct := DNSZoneResponse{
+					Name:            *zone.Name,
+					ID:              *zone.ID,
+					CISInstanceCRN:  *instance.CRN,
+					CISInstanceName: *instance.Name,
+					ResourceGroupID: *instance.ResourceGroupID,
+				}
+				allZones = append(allZones, zoneStruct)
+			}
+		}
+	}
+
+	return allZones, nil
+}
+
+func (c *Client) loadResourceManagementAPI() error {
+	authenticator := &core.IamAuthenticator{
+		ApiKey: c.ApiKey,
+	}
+	options := &resourcemanagerv2.ResourceManagerV2Options{
+		Authenticator: authenticator,
+	}
+	resourceManagerV2Service, err := resourcemanagerv2.NewResourceManagerV2(options)
+	if err != nil {
+		return err
+	}
+	c.managementAPI = resourceManagerV2Service
+	return nil
+}
+
+func (c *Client) loadResourceControllerAPI() error {
+	authenticator := &core.IamAuthenticator{
+		ApiKey: c.ApiKey,
+	}
+	options := &resourcecontrollerv2.ResourceControllerV2Options{
+		Authenticator: authenticator,
+	}
+	resourceControllerV2Service, err := resourcecontrollerv2.NewResourceControllerV2(options)
+	if err != nil {
+		return err
+	}
+	c.controllerAPI = resourceControllerV2Service
+	return nil
+}
+
+func (c *Client) loadVPCV1API() error {
+	authenticator := &core.IamAuthenticator{
+		ApiKey: c.ApiKey,
+	}
+	vpcService, err := vpcv1.NewVpcV1(&vpcv1.VpcV1Options{
+		Authenticator: authenticator,
+	})
+	if err != nil {
+		return err
+	}
+	c.vpcAPI = vpcService
+	return nil
+}
+
+func main() {
+	client, err := NewClient()
+	if err != nil {
+		log.Printf("failed to get IBM Cloud client: %v", err)
+		os.Exit(1)
+	}
+
+	zoneID, err := client.GetDNSZoneIDByName(context.TODO(), "scnl-ibm.com")
+	if err != nil {
+		log.Printf("failed to get DNS zone ID: %v", err)
+		os.Exit(1)
+	}
+	log.Printf("zoneID = %v", zoneID)
+}
