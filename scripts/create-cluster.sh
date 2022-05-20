687b3bfa87f07eeb60732274b74521175f66f37b
Mark Hamzy <hamzy@us.ibm.com>
2022-05-20 10:42:34 -0500

    Remove old hack functions

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -19,211 +19,12 @@ function log_to_file()
 	exec 2>&1
 }
 
-function create_ibm_cloud_credentials_secret()
-{
-	log_to_file /tmp/create-ibm-cloud-credentials-secret.log
-	RC=1
-	while [ ${RC} -gt 0 ]
-	do
-		sleep 1m
-		set +e
-		oc --request-timeout=10s get secret/cloud-credentials -n openshift-ingress-operator > /dev/null
-		RC=$?
-		set -e
-		echo "RC=${RC}"
-	done
-	oc get secret/cloud-credentials -n openshift-ingress-operator -o json \
-		| sed	-e 's,"cloud-credentials","ibm-cloud-credentials",' \
-			-e 's,"openshift-ingress-operator","openshift-cloud-controller-manager",' \
-		| oc create -f -
-	echo "create_ibm_cloud_credentials_secret: FINISHED!"
-}
-
 function init_ibmcloud()
 {
 	if ! ibmcloud iam oauth-tokens 1>/dev/null 2>&1
 	then
 		ibmcloud login --apikey "${IBMCLOUD_API_KEY}" -r ${VPCREGION}
 	fi
-}
-
-function fix_load_balancer_hostname()
-{	
-	log_to_file /tmp/fix-load-balancer-hostname.log
-
-	CLUSTER_ID=$(jq -r '.infraID' ${CLUSTER_DIR}/metadata.json)
-	HOSTNAME_EXTERNAL="apps.${CLUSTER_NAME}.scnl-ibm.com"
-
-	FOUND=false
-	FILE=$(mktemp)
-
-	while ! ${FOUND}
-	do
-		sleep 1m
-		ibmcloud is load-balancers --json | jq -r '.[] | select (.name|test("^kube-'${CLUSTER_ID}'"))' > ${FILE}
-		RC=$?
-		if [ ${RC} -eq 0 ]
-		then
-			LINE_OUTPUT=$(wc -l ${FILE})
-			RC=$?
-			if [ ${RC} -eq 0 ]
-			then
-				LINES=$(echo "${LINE_OUTPUT}" | cut -f1 -d' ')
-				RC=$?
-				if (( ${LINES} > 0 ))
-				then
-					FOUND=true
-				fi
-			fi
-		fi
-	done
-
-	HOSTNAME_LB=$(ibmcloud is load-balancers --json | jq -r '.[] | select (.name|test("^kube-'${CLUSTER_ID}'")) | .hostname')
-	ID_DOMAIN=$(ibmcloud cis domains --output json | jq -r '.[] | select (.name|test("^scnl-ibm.com$")) | .id')
-	ID_HOSTNAME=$(ibmcloud cis dns-records ${ID_DOMAIN} --output json | jq -r '.[] | select (.name|test("'${HOSTNAME_EXTERNAL}'$")) | .id')
-	if [ -n "${ID_HOSTNAME}" ]
-	then
-		ibmcloud cis dns-record-update ${ID_DOMAIN} ${ID_HOSTNAME} --json '{ "name": "*.'${HOSTNAME_EXTERNAL}'", "type": "CNAME", "content": "'${HOSTNAME_LB}'" }'
-	else
-		ibmcloud cis dns-record-create ${ID_DOMAIN} --json '{ "name": "*.'${HOSTNAME_EXTERNAL}'", "type": "CNAME", "content": "'${HOSTNAME_LB}'" }'
-	fi
-	/bin/rm ${FILE}
-	echo "fix_load_balancer_hostname: FINISHED!"
-}
-
-function delete_wildcard_dns()
-{	
-	log_to_file /tmp/delete-wildcard-dns.log
-
-	HOSTNAME_WILDCARD="apps.${CLUSTER_NAME}.scnl-ibm.com"
-	ID_DOMAIN=$(ibmcloud cis domains --output json | jq -r '.[] | select (.name|test("^scnl-ibm.com$")) | .id')
-
-	FOUND=false
-	FILE=$(mktemp)
-
-	while ! ${FOUND}
-	do
-		sleep 1m
-
-		ibmcloud cis dns-records ${ID_DOMAIN} --output json > ${FILE}
-		RC=$?
-		if [ ${RC} -gt 0 ]
-		then
-			continue
-		fi
-
-		ibmcloud cis dns-records ${ID_DOMAIN} --output json | jq -r '.[] | select (.name|test("'${HOSTNAME_WILDCARD}'$"))' > ${FILE}
-		RC=$?
-		if [ ${RC} -eq 0 ]
-		then
-			LINE_OUTPUT=$(wc -l ${FILE})
-			RC=$?
-			if [ ${RC} -eq 0 ]
-			then
-				LINES=$(echo "${LINE_OUTPUT}" | cut -f1 -d' ')
-				RC=$?
-				if (( ${LINES} > 0 ))
-				then
-					FOUND=true
-				fi
-			fi
-		fi
-
-		if ${FOUND}
-		then
-			ID_WILDCARD=$(jq -r --slurp '.[0].id' ${FILE})
-			ibmcloud cis dns-record-delete ${ID_DOMAIN} ${ID_WILDCARD}
-		fi
-	done
-
-	/bin/rm ${FILE}
-	echo "delete_wildcard_dns: FINISHED!"
-}
-
-function reboot_master_nodes()
-{	
-	log_to_file /tmp/reboot-master-nodes.log
-
-	CLUSTER_ID=$(jq -r '.infraID' ${CLUSTER_DIR}/metadata.json)
-
-	FOUND=false
-	FILE=$(mktemp)
-
-	while ! ${FOUND}
-	do
-		ibmcloud is load-balancers --json | jq -r '.[] | select (.name|test("^'${CLUSTER_ID}'-loadbalancer$"))' > ${FILE}
-		RC=$?
-		if [ ${RC} -eq 0 ]
-		then
-			LINE_OUTPUT=$(wc -l ${FILE})
-			RC=$?
-			if [ ${RC} -eq 0 ]
-			then
-				LINES=$(echo "${LINE_OUTPUT}" | cut -f1 -d' ')
-				RC=$?
-				if (( ${LINES} > 0 ))
-				then
-					FOUND=true
-				fi
-			fi
-		else
-			sleep 1m
-		fi
-	done
-
-	LB_ID=$(ibmcloud is load-balancers --json | jq -r '.[] | select (.name|test("^'${CLUSTER_ID}'-loadbalancer$")) | .id')
-
-	READY=false
-
-	while ! ${READY}
-	do
-		STATUS=$(ibmcloud is load-balancers --json | jq -r '.[] | select (.id|test("'${LB_ID}'")) | .operating_status')
-		if [ "${STATUS}" == "online" ]
-		then
-			READY=true
-		fi
-	done
-
-	SELECT="${CLUSTER_ID}-master"
-	W_FILE=$(mktemp)
-	
-	while true
-	do
-		ibmcloud pi ins --json | jq '.Payload.pvmInstances[] | select (.serverName|test("'${SELECT}'"))' > ${W_FILE}
-	
-		MASTER0=$(jq -r '. | select(.serverName|test("master-0")) | .addresses[].ip' ${W_FILE})
-		MASTER1=$(jq -r '. | select(.serverName|test("master-1")) | .addresses[].ip' ${W_FILE})
-		MASTER2=$(jq -r '. | select(.serverName|test("master-2")) | .addresses[].ip' ${W_FILE})
-
-		if [ -n "${MASTER0}" ] && [ -n "${MASTER1}" ] && [ -n "${MASTER2}" ]
-		then
-			break
-		fi
-
-		sleep 1m
-	done
-
-	OUTPUT0=$(ssh cloud-user@161.156.204.62 ssh-keyscan ${MASTER0} || true)
-	OUTPUT1=$(ssh cloud-user@161.156.204.62 ssh-keyscan ${MASTER1} || true)
-	OUTPUT2=$(ssh cloud-user@161.156.204.62 ssh-keyscan ${MASTER2} || true)
-
-	if [ -z "${OUTPUT0}" ]
-	then
-		echo ibmcloud pi instance-hard-reboot "${CLUSTER_ID}-master-0"
-	fi
-
-	if [ -z "${OUTPUT1}" ]
-	then
-		echo ibmcloud pi instance-hard-reboot "${CLUSTER_ID}-master-1"
-	fi
-
-	if [ -z "${OUTPUT2}" ]
-	then
-		echo ibmcloud pi instance-hard-reboot "${CLUSTER_ID}-master-2"
-	fi
-
-	/bin/rm ${FILE} ${W_FILE}
-	echo "reboot_master_nodes: FINISHED!"
 }
 
 declare -a ENV_VARS
@@ -250,9 +51,6 @@ set -euo pipefail
 export IBMCLOUD_REGION=${POWERVS_REGION}
 export IBMCLOUD_ZONE=${POWERVS_ZONE}
 
-#export OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE="quay.io/psundara/openshift-release:4.10-powervs"
-#export OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE="quay.io/openshift-release-dev/ocp-release-nightly:4.11.0-0.nightly-ppc64le-2022-05-06-093203"
-#export OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE="quay.io/openshift-release-dev/ocp-release-nightly:4.11.0-0.nightly-ppc64le-2022-05-10-222820"
 export OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE="quay.io/openshift-release-dev/ocp-release-nightly:4.11.0-0.nightly-ppc64le-2022-05-13-170447"
 
 export PATH=${PATH}:$(pwd)/bin
@@ -267,7 +65,9 @@ export IC_API_KEY=${IBMCLOUD_API_KEY}
 
 set -x
 
+#
 # Quota check DNS
+#
 export DNS_DOMAIN_ID=$(ibmcloud cis domains --output json | jq -r '.[].id')
 RECORDS=$(ibmcloud cis dns-records ${DNS_DOMAIN_ID} --output json | jq -r '.[] | select (.name|test("'${CLUSTER_NAME}'.*")) | "\(.name) - \(.id)"')
 if [ -n "${RECORDS}" ]
@@ -276,23 +76,25 @@ then
 	exit 1
 fi
 
+#
 # Quota check cloud connections
+#
 CONNECTIONS=$(ibmcloud pi connections --json | jq -r '.Payload.cloudConnections|length')
-if (( ${CONNECTIONS} >= 2 ))
+if (( ${CONNECTIONS} >= 1 ))
 then
-	echo "Error: Cannot have 2 or more cloud connections.  You currently have ${CONNECTIONS}."
+	echo "Error: Cannot have 1 or more cloud connections.  You currently have ${CONNECTIONS}."
 	exit 1
 fi
 
+#
 # Quota check for image imports
+#
 JOBS=$(ibmcloud pi jobs --operation-action imageImport --json | jq -r '.Payload.jobs[] | select (.status.state|test("running")) | .id')
 if [ -n "${JOBS}" ]
 then
 	echo "${JOBS}"
 	exit 1
 fi
-
-#export SERVICE_INSTANCE_ID=$(ibmcloud resource service-instance ${SERVICE_INSTANCE} --output json | jq -r '.[].guid')
 
 declare -a JOBS
 
@@ -414,14 +216,5 @@ date
 openshift-install create cluster --dir ${CLUSTER_DIR} --log-level=debug &
 PID_INSTALL=$!
 JOBS+=( "${PID_INSTALL}" )
-
-#create_ibm_cloud_credentials_secret &
-#JOBS+=( "$!" )
-
-#fix_load_balancer_hostname &
-#JOBS+=( "$!" )
-
-#delete_wildcard_dns &
-#JOBS+=( "$!" )
 
 wait ${PID_INSTALL}
