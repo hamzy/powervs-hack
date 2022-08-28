04cbaa574846911238fc1f36bb4b93c546eba8d8
Mark Hamzy <hamzy@us.ibm.com>
2022-08-28 17:14:05 -0500

    Latest code

--- list-service-instances/list-service-instances.go
+++ list-service-instances/list-service-instances.go
@@ -19,15 +19,15 @@ import (
 	"github.com/IBM/go-sdk-core/v5/core"
 	"github.com/IBM/platform-services-go-sdk/resourcecontrollerv2"
 	"log"
+	gohttp "net/http"
 	"os"
 	"regexp"
 )
 
-const cosTypeName = "cos instance"
-const resourceGroupID = "powervs-ipi-resource-group"
-const cosResourceID = "dff97f5c-bc5e-4455-b470-411c3edbe49c"
+const resourceGroupID = "c1cb9b2679344ee9951ab8b4bc22eca0"     // "powervs-ipi-resource-group"
+const cosResourceID   = "dff97f5c-bc5e-4455-b470-411c3edbe49c" //
 
-func listServiceInstances (rSearch *regexp.Regexp, controllerSvc *resourcecontrollerv2.ResourceControllerV2, context context.Context) {
+func listServiceInstances1 (rSearch *regexp.Regexp, controllerSvc *resourcecontrollerv2.ResourceControllerV2, context context.Context) {
 
 	options := controllerSvc.NewListResourceInstancesOptions()
 //	options.SetResourceGroupID(resourceGroupID)
@@ -40,10 +40,89 @@ func listServiceInstances (rSearch *regexp.Regexp, controllerSvc *resourcecontro
 	}
 
 	for _, resource := range resources.Resources {
-		if rSearch.MatchString(*resource.Name) {
+//		if rSearch.MatchString(*resource.Name) {
 			log.Printf("resource: %s\n", *resource.Name)
-		}
+//		}
 	}
+}
+
+func listServiceInstances2 (rSearch *regexp.Regexp, controllerSvc *resourcecontrollerv2.ResourceControllerV2, context context.Context) {
+
+	var options *resourcecontrollerv2.ListResourceInstancesOptions
+	var resources *resourcecontrollerv2.ResourceInstancesList
+	var err error
+	var perPage int64 = 10
+	var moreData bool = true
+	var nextURL *string
+
+	options = controllerSvc.NewListResourceInstancesOptions()
+//	options.SetType("resource_instance")
+//	options.SetType("service_instance")
+//	options.SetResourceID(cosResourceID)
+//	options.SetResourceGroupID(resourceGroupID)
+	options.SetLimit(perPage)
+
+	for moreData {
+
+		log.Printf("options = %+v\n", options)
+		log.Printf("options.Limit = %v\n", *options.Limit)
+		if options.Start != nil {
+			log.Printf("optionsStart = %v\n", *options.Start)
+		}
+		resources, _, err = controllerSvc.ListResourceInstancesWithContext(context, options)
+		if err != nil {
+			log.Fatalf("Failed to list resource instances: %v", err)
+		}
+
+		log.Printf("resources.RowsCount = %v\n", *resources.RowsCount)
+
+		for _, resource := range resources.Resources {
+			var getResourceOptions *resourcecontrollerv2.GetResourceInstanceOptions
+			var resourceInstance *resourcecontrollerv2.ResourceInstance
+			var response *core.DetailedResponse
+
+			log.Printf("resource: %s\n", *resource.Name)
+
+			getResourceOptions = controllerSvc.NewGetResourceInstanceOptions(*resource.ID)
+
+			resourceInstance, response, err = controllerSvc.GetResourceInstance(getResourceOptions)
+			if err != nil {
+				log.Fatalf("Failed to get instance: %v", err)
+				continue
+			}
+			if err != nil && response != nil && response.StatusCode == gohttp.StatusNotFound {
+				log.Printf("gohttp.StatusNotFound\n")
+				continue
+			}
+			if err != nil && response != nil && response.StatusCode == gohttp.StatusInternalServerError {
+				log.Printf("gohttp.StatusInternalServerError\n")
+				continue
+			}
+
+			if resourceInstance.Type == nil {
+				log.Printf("type: nil\n")
+			} else {
+				log.Printf("type: %v\n", *resourceInstance.Type)
+			}
+			if resourceInstance.SubType == nil {
+				log.Printf("sub type: nil\n")
+			} else {
+				log.Printf("sub type: %v\n", *resourceInstance.SubType)
+			}
+		}
+
+		// Based on: https://cloud.ibm.com/apidocs/resource-controller/resource-controller?code=go#list-resource-instances
+		nextURL, err = core.GetQueryParam(resources.NextURL, "start")
+		if err != nil {
+			log.Fatalf("Failed to GetQueryParam on start: %v", err)
+		}
+		log.Printf("nextURL = %v\n", *nextURL)
+		options.SetStart(*nextURL)
+
+		moreData = *resources.RowsCount == perPage
+
+	}
+
 }
 
 func main() {
@@ -66,12 +145,12 @@ func main() {
 		Authenticator: &core.IamAuthenticator{
 			ApiKey: apiKey,
 		},
-		ServiceName: "cloud-object-storage",
-		URL: "https://resource-controller.cloud.ibm.com",
+//		ServiceName: "cloud-object-storage",
+//		URL: "https://resource-controller.cloud.ibm.com",
 	})
 	if err != nil {
 		log.Fatal("Error creating ControllerV2 Service.")
 	}
 
-	listServiceInstances (rSearch, controllerSvc, context)
+	listServiceInstances2 (rSearch, controllerSvc, context)
 }
