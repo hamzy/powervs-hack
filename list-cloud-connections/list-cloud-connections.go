c55c2085d7d2c0c797d2f7b338fb916101d644f5
Mark Hamzy <hamzy@us.ibm.com>
2022-09-14 13:48:22 -0500

    Add list-cloud-connections

---
+++ list-cloud-connections/list-cloud-connections.go
@@ -0,0 +1,411 @@
+// Copyright 2021 IBM Corp
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// https://github.com/openshift/installer/commit/b5c77e12acc6841d08378a8a5d45ea0f6aa1dd93
+
+// package vpc1
+// https://raw.githubusercontent.com/IBM/vpc-go-sdk/master/vpcv1/vpc_v1.go
+
+// package resourcecontrollerv2
+// https://raw.githubusercontent.com/IBM/platform-services-go-sdk/main/resourcecontrollerv2/resource_controller_v2.go
+
+// package resourcemanagerv2
+// https://raw.githubusercontent.com/IBM/platform-services-go-sdk/main/resourcemanagerv2/resource_manager_v2.go
+
+// package dnsrecordsv1
+// https://raw.githubusercontent.com/IBM/networking-go-sdk/master/dnsrecordsv1/dns_records_v1.go
+
+// How to run:
+// (export IBMCLOUD_API_KEY="blah"; if ! ibmcloud iam oauth-tokens 1>/dev/null 2>&1; then ibmcloud login --apikey "${IBMCLOUD_API_KEY}"; fi; go run list-jobs.go -apiKey "${IBMCLOUD_API_KEY}" -search '.*rdr-hamzy-.*' -serviceName powervs-ipi-lon04 -shouldDebug false -shouldDelete false)
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/golang-jwt/jwt"
+	"github.com/IBM-Cloud/bluemix-go"
+	"github.com/IBM-Cloud/bluemix-go/api/resource/resourcev2/controllerv2"
+	"github.com/IBM-Cloud/bluemix-go/authentication"
+	"github.com/IBM-Cloud/bluemix-go/http"
+	"github.com/IBM-Cloud/bluemix-go/rest"
+	bxsession "github.com/IBM-Cloud/bluemix-go/session"
+	"github.com/IBM-Cloud/power-go-client/clients/instance"
+	"github.com/IBM-Cloud/power-go-client/ibmpisession"
+	"github.com/IBM-Cloud/power-go-client/power/models"
+	"github.com/IBM/go-sdk-core/v5/core"
+	"log"
+	gohttp "net/http"
+	"regexp"
+	"strings"
+)
+
+var shouldDebug = false
+var shouldDelete = false
+
+// $ ibmcloud catalog service cloud-object-storage --output json | jq -r '.[].id'
+// dff97f5c-bc5e-4455-b470-411c3edbe49c
+const cosResourceID = "dff97f5c-bc5e-4455-b470-411c3edbe49c"
+
+type User struct {
+	ID         string
+	Email      string
+	Account    string
+	cloudName  string `default:"bluemix"`
+	cloudType  string `default:"public"`
+	generation int    `default:"2"`
+}
+
+func fetchUserDetails(bxSession *bxsession.Session, generation int) (*User, error) {
+	config := bxSession.Config
+	user := User{}
+	var bluemixToken string
+
+	if strings.HasPrefix(config.IAMAccessToken, "Bearer") {
+		bluemixToken = config.IAMAccessToken[7:len(config.IAMAccessToken)]
+	} else {
+		bluemixToken = config.IAMAccessToken
+	}
+
+	token, err := jwt.Parse(bluemixToken, func(token *jwt.Token) (interface{}, error) {
+		return "", nil
+	})
+	if err != nil && !strings.Contains(err.Error(), "key is of invalid type") {
+		return &user, err
+	}
+
+	claims := token.Claims.(jwt.MapClaims)
+	if email, ok := claims["email"]; ok {
+		user.Email = email.(string)
+	}
+	user.ID = claims["id"].(string)
+	user.Account = claims["account"].(map[string]interface{})["bss"].(string)
+	iss := claims["iss"].(string)
+	if strings.Contains(iss, "https://iam.cloud.ibm.com") {
+		user.cloudName = "bluemix"
+	} else {
+		user.cloudName = "staging"
+	}
+	user.cloudType = "public"
+
+	user.generation = generation
+	return &user, nil
+}
+
+func GetRegion(zone string) (region string, err error) {
+	err = nil
+	switch {
+	case strings.HasPrefix(zone, "dal"), strings.HasPrefix(zone, "us-south"):
+		region = "us-south"
+	case strings.HasPrefix(zone, "sao"):
+		region = "sao"
+	case strings.HasPrefix(zone, "us-east"):
+		region = "us-east"
+	case strings.HasPrefix(zone, "tor"):
+		region = "tor"
+	case strings.HasPrefix(zone, "eu-de-"):
+		region = "eu-de"
+	case strings.HasPrefix(zone, "lon"):
+		region = "lon"
+	case strings.HasPrefix(zone, "syd"):
+		region = "syd"
+	case strings.HasPrefix(zone, "tok"):
+		region = "tok"
+	case strings.HasPrefix(zone, "osa"):
+		region = "osa"
+	case strings.HasPrefix(zone, "mon"):
+		region = "mon"
+	default:
+		return "", fmt.Errorf("region not found for the zone: %s", zone)
+	}
+	return
+}
+
+func listCloudConnections (rSearch *regexp.Regexp, cloudConnectionClient *instance.IBMPICloudConnectionClient, serviceGuid string) {
+	var (
+		// https://github.com/IBM-Cloud/power-go-client/blob/v1.0.88/power/models/cloud_connections.go#L20-L25
+		cloudConnections *models.CloudConnections
+
+		// https://github.com/IBM-Cloud/power-go-client/blob/v1.0.88/power/models/cloud_connection.go#L20-L71
+		cloudConnection *models.CloudConnection
+
+		err error
+
+		Context context.Context = context.Background()
+
+		// https://github.com/IBM-Cloud/power-go-client/blob/master/power/models/cloud_connection_endpoint_v_p_c.go#L20
+		EndpointVpc *models.CloudConnectionEndpointVPC
+
+		// https://github.com/IBM-Cloud/power-go-client/blob/master/power/models/cloud_connection_v_p_c.go#L20
+		Vpc *models.CloudConnectionVPC
+
+		foundVpc bool = false
+	)
+
+	if shouldDebug {
+		log.Printf("Listing Cloud Connections")
+	}
+
+	select {
+	case <-Context.Done():
+		if shouldDebug {
+			log.Printf("listCloudConnections: case <-Context.Done()")
+		}
+		return // we're cancelled, abort
+	default:
+	}
+
+	cloudConnections, err = cloudConnectionClient.GetAll()
+	if err != nil {
+		log.Fatalf("Failed to list cloud connections: %v", err)
+	}
+
+	for _, cloudConnection = range cloudConnections.CloudConnections {
+		if shouldDebug {
+			log.Printf("listCloudConnections: FOUND: %s (%s)", *cloudConnection.Name, *cloudConnection.CloudConnectionID)
+		}
+
+		if rSearch.MatchString(*cloudConnection.Name) {
+			if shouldDebug {
+				log.Printf("listCloudConnections: EndpointVpc = %+v\n", cloudConnection.Vpc)
+			}
+
+			// Unfortunately, the enumerated Cloud Connections are missing their VPC information
+			// so requery the CC
+			cloudConnection, err = cloudConnectionClient.Get(*cloudConnection.CloudConnectionID)
+			if err != nil {
+				log.Fatalf("Failed to get cloud connection %s: %v", cloudConnection.CloudConnectionID, err)
+			}
+
+			EndpointVpc = cloudConnection.Vpc
+			if shouldDebug {
+				log.Printf("listCloudConnections: EndpointVpc = %+v\n", EndpointVpc)
+			}
+
+			for _, Vpc = range EndpointVpc.Vpcs {
+				if Vpc != nil {
+					foundVpc = true
+				}
+				if shouldDebug {
+					log.Printf("listCloudConnections: Vpc = %+v\n", Vpc)
+				}
+			}
+			if shouldDebug {
+				log.Printf("listCloudConnections: foundVpc = %v\n", foundVpc)
+			}
+
+			if !shouldDelete {
+				if shouldDebug {
+					log.Printf("Skipping deleting cloud connection %q since shouldDelete is false", *cloudConnection.Name)
+				}
+				continue
+			}
+		}
+	}
+}
+
+func createPiSession (ptrApiKey *string, ptrServiceName *string) (*ibmpisession.IBMPISession, string, error) {
+
+	var bxSession *bxsession.Session
+	var tokenProviderEndpoint string = "https://iam.cloud.ibm.com"
+	var err error
+
+	bxSession, err = bxsession.New(&bluemix.Config{
+		BluemixAPIKey:         *ptrApiKey,
+		TokenProviderEndpoint: &tokenProviderEndpoint,
+		Debug:                 false,
+	})
+	if err != nil {
+		return nil, "", fmt.Errorf("Error bxsession.New: %v", err)
+	}
+	if shouldDebug {
+		log.Printf("bxSession = %v\n", bxSession)
+	}
+
+	tokenRefresher, err := authentication.NewIAMAuthRepository(bxSession.Config, &rest.Client{
+		DefaultHeader: gohttp.Header{
+			"User-Agent": []string{http.UserAgent()},
+		},
+	})
+	if err != nil {
+		return nil, "", fmt.Errorf("Error authentication.NewIAMAuthRepository: %v", err)
+	}
+	if shouldDebug {
+		log.Printf("tokenRefresher = %v\n", tokenRefresher)
+	}
+	err = tokenRefresher.AuthenticateAPIKey(bxSession.Config.BluemixAPIKey)
+	if err != nil {
+		return nil, "", fmt.Errorf("Error tokenRefresher.AuthenticateAPIKey: %v", err)
+	}
+
+	user, err := fetchUserDetails(bxSession, 2)
+	if err != nil {
+		return nil, "", fmt.Errorf("Error fetchUserDetails: %v", err)
+	}
+
+	ctrlv2, err := controllerv2.New(bxSession)
+	if err != nil {
+		return nil, "", fmt.Errorf("Error controllerv2.New: %v", err)
+	}
+	if shouldDebug {
+		log.Printf("ctrlv2 = %v\n", ctrlv2)
+	}
+
+	resourceClientV2 := ctrlv2.ResourceServiceInstanceV2()
+	if err != nil {
+		return nil, "", fmt.Errorf("Error ctrlv2.ResourceServiceInstanceV2: %v", err)
+	}
+	if shouldDebug {
+		log.Printf("resourceClientV2 = %v\n", resourceClientV2)
+	}
+
+	svcs, err := resourceClientV2.ListInstances(controllerv2.ServiceInstanceQuery{
+		Type: "service_instance",
+	})
+	if err != nil {
+		return nil, "", fmt.Errorf("Error resourceClientV2.ListInstances: %v", err)
+	}
+
+	var serviceGuid string = ""
+
+	for _, svc := range svcs {
+		if shouldDebug {
+			log.Printf("Guid = %v\n", svc.Guid)
+			log.Printf("RegionID = %v\n", svc.RegionID)
+			log.Printf("Name = %v\n", svc.Name)
+			log.Printf("Crn = %v\n", svc.Crn)
+		}
+		if svc.Name == *ptrServiceName {
+			serviceGuid = svc.Guid
+			break
+		}
+	}
+	if serviceGuid == "" {
+		return nil, "", fmt.Errorf("%s not found in list of service instances!\n", *ptrServiceName)
+	}
+	if shouldDebug {
+		log.Printf("serviceGuid = %v\n", serviceGuid)
+	}
+
+	serviceInstance, err := resourceClientV2.GetInstance(serviceGuid)
+	if err != nil {
+		return nil, "", fmt.Errorf("Error resourceClientV2.GetInstance: %v", err)
+	}
+	if shouldDebug {
+		log.Printf("serviceInstance = %v\n", serviceInstance)
+
+	}
+
+	region, err:= GetRegion(serviceInstance.RegionID)
+	if err != nil {
+		return nil, "", fmt.Errorf("Error GetRegion: %v", err)
+	}
+
+	var authenticator core.Authenticator = &core.IamAuthenticator{
+		ApiKey:	*ptrApiKey,
+	}
+
+	err = authenticator.Validate()
+	if err != nil {
+		return nil, "", fmt.Errorf("Error: authenticator.Validate: %v", err)
+	}
+
+	var options *ibmpisession.IBMPIOptions = &ibmpisession.IBMPIOptions{
+		Authenticator: authenticator,
+		Debug:         false,
+		Region:        region,
+		UserAccount:   user.Account,
+		Zone:          serviceInstance.RegionID,
+	}
+
+	var piSession *ibmpisession.IBMPISession
+
+	piSession, err = ibmpisession.NewIBMPISession(options)
+	if (err != nil) || (piSession == nil) {
+		if err != nil {
+			return nil, "", fmt.Errorf("Error: ibmpisession.NewIBMPISession: %v", err)
+		}
+		return nil, "", fmt.Errorf("Error: piSession is nil")
+	}
+	if shouldDebug {
+		log.Printf("piSession = %v\n", piSession)
+	}
+
+	return piSession, serviceGuid, nil
+
+}
+
+func main() {
+
+	var ptrApiKey *string
+	var ptrSearch *string
+	var ptrServiceName *string
+	var ptrShouldDebug *string
+	var ptrShouldDelete *string
+
+	ptrApiKey = flag.String("apiKey", "", "Your IBM Cloud API key")
+	ptrSearch = flag.String("search", "", "The search string to match for deletes")
+	ptrServiceName = flag.String("serviceName", "", "The cloud service to use")
+	ptrShouldDebug = flag.String("shouldDebug", "false", "Should output debug output")
+	ptrShouldDelete = flag.String("shouldDelete", "false", "Should delete matching records")
+
+	flag.Parse()
+
+	if *ptrApiKey == "" {
+		log.Fatal("Error: No API key set, use --apiKey")
+	}
+
+	if *ptrSearch == "" {
+		log.Fatal("Error: No search term set, use --search")
+	}
+
+	if *ptrServiceName == "" {
+		log.Fatal("Error: No cloud service set, use --serviceName")
+	}
+	switch strings.ToLower(*ptrShouldDebug) {
+	case "true":
+		shouldDebug = true
+	case "false":
+		shouldDebug = false
+	default:
+		log.Fatal("Error: shouldDebug is not true/false (%s)\n", *ptrShouldDebug)
+	}
+	switch strings.ToLower(*ptrShouldDelete) {
+	case "true":
+		shouldDelete = true
+	case "false":
+		shouldDelete = false
+	default:
+		log.Fatal("Error: shouldDelete is not true/false (%s)\n", *ptrShouldDelete)
+	}
+
+	rSearch, _ := regexp.Compile(*ptrSearch)
+
+	var piSession *ibmpisession.IBMPISession
+	var serviceGuid string
+	var err error
+
+	piSession, serviceGuid, err = createPiSession(ptrApiKey, ptrServiceName)
+	if err != nil {
+		log.Fatal("Error createPiSession: %v\n", err)
+	}
+
+	var cloudConnectionClient *instance.IBMPICloudConnectionClient
+
+	cloudConnectionClient = instance.NewIBMPICloudConnectionClient(context.Background(), piSession, serviceGuid)
+	if shouldDebug { log.Printf("cloudConnectionClient = %v\n", cloudConnectionClient) }
+
+	listCloudConnections(rSearch, cloudConnectionClient, serviceGuid)
+}
