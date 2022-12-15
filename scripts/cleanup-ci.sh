0448bbab8498b266f527ff1714a8a0cfd7ffc8ab
Mark Hamzy <hamzy@us.ibm.com>
2022-12-15 10:27:54 -0600

    Add support for destroy-cluster3

--- scripts/cleanup-ci.sh
+++ scripts/cleanup-ci.sh
@@ -20,9 +20,13 @@ done
 
 set -xeuo pipefail
 
-USE_POWERVS_HACK=true
+USE_POWERVS_HACK2=true
+USE_POWERVS_HACK3=false
 
-if ${USE_POWERVS_HACK}
+if ${USE_POWERVS_HACK2}
+then
+	hash destroy-cluster2
+elif ${USE_POWERVS_HACK3}
 then
 	hash destroy-cluster3
 else
@@ -36,7 +40,8 @@ export BASEDOMAIN="ocp-dev-ppc64le.com"
 export CIS_INSTANCE="pvs-ipi-dns"
 export RESOURCE_GROUP_ID="ba5e48e53192476092e188e0e0c6eb9e"
 
-for SERVICE_INSTANCE in "ocp-ipi-ci-mon01" "ocp-ipi-ci-osa21" "ocp-ipi-ci-sao01" "ocp-ipi-ci-syd04" "ocp-ipi-ci-syd05" "ocp-ipi-ci-tok04" "ocp-ipi-ci-tor01"
+# for SERVICE_INSTANCE in "ocp-ipi-ci-mon01" "ocp-ipi-ci-osa21" "ocp-ipi-ci-sao01" "ocp-ipi-ci-syd04" "ocp-ipi-ci-syd05" "ocp-ipi-ci-tok04" "ocp-ipi-ci-tor01"
+for SERVICE_INSTANCE in "ocp-ipi-ci-mon01" "ocp-ipi-ci-osa21" "ocp-ipi-ci-syd04" "ocp-ipi-ci-syd05" "ocp-ipi-ci-tok04" "ocp-ipi-ci-tor01"
 do
 
 	case ${SERVICE_INSTANCE} in
@@ -84,7 +89,8 @@ do
 			;;
 	esac
 
-	export CLUSTER_NAME="rdr-multiarch-${POWERVS_ZONE}"
+	export CLUSTER_NAME="rdr-maocp-${POWERVS_ZONE}"
+	#export CLUSTER_NAME="rdr-multiarch-${POWERVS_ZONE}"
 
 	ibmcloud logout
 	ibmcloud login --apikey "${IBMCLOUD_API_KEY}" -r "${POWERVS_REGION}"
@@ -94,33 +100,47 @@ do
 	export CIS_INSTANCE_CRN=$(ibmcloud cis instances --output json | jq -r '.[] | select (.name|test("'${CIS_INSTANCE}'")) | .crn')
 	export SERVICE_INSTANCE_GUID=$(ibmcloud resource service-instance ${SERVICE_INSTANCE} --output json | jq -r '.[].guid')
 
+	export DNS_INSTANCE_CRN=""
+
 	for ((I=1; I <= 3; I++))
 	do
+
+		if ${USE_POWERVS_HACK2}
+		then
+			USE_POWERVS_HACK=true
+			CMD=destroy-cluster2
+			INSTANCE_CRN="-CISInstanceCRN ${CIS_INSTANCE_CRN}"
+		elif ${USE_POWERVS_HACK3}
+		then
+			USE_POWERVS_HACK=true
+			CMD=destroy-cluster3
+			INSTANCE_CRN="-CISInstanceCRN ${CIS_INSTANCE_CRN} -DNSInstanceCRN ${DNS_INSTANCE_CRN}"
+		fi
 
 		if ${USE_POWERVS_HACK}
 		then
 
 			set +e
-			destroy-cluster3 \
+			${CMD} \
 				-apiKey "${IBMCLOUD_API_KEY}" \
 				-baseDomain "ocp-dev-ppc64le.com" \
 				-clusterName "${CLUSTER_NAME}" \
 				-infraID "${CLUSTER_NAME}" \
-				-CISInstanceCRN "${CIS_INSTANCE_CRN}" \
+				${INSTANCE_CRN} \
 				-region "${POWERVS_REGION}" \
 				-zone "${POWERVS_ZONE}" \
 				-serviceInstanceGUID "${SERVICE_INSTANCE_GUID}" \
 				-resourceGroupID "${RESOURCE_GROUP_ID}" \
 				-shouldDebug true \
 				-shouldDelete true \
-				-shouldDeleteDHCP true
+				-shouldDeleteDHCP false
 			RC=$?
 			set -e
 
 		else
 
 			cat << ___EOF___ > ${SAVE_DIR}/metadata.json
-{"clusterName":"${CLUSTER_NAME}","clusterID":"","infraID":"${CLUSTER_NAME}","powervs":{"BaseDomain":"${BASEDOMAIN}","cisInstanceCRN":"${CIS_INSTANCE_CRN}","powerVSResourceGroup":"${RESOURCE_GROUP_ID}","region":"${POWERVS_REGION}","vpcRegion":"","zone":"${POWERVS_ZONE}","serviceInstanceID":"${SERVICE_INSTANCE_GUID}"}}
+{"clusterName":"${CLUSTER_NAME}","clusterID":"","infraID":"${CLUSTER_NAME}","powervs":{"BaseDomain":"${BASEDOMAIN}","cisInstanceCRN":"${CIS_INSTANCE_CRN}","dnsInstanceCRN": "${DNS_INSTANCE_CRN}", "powerVSResourceGroup":"${RESOURCE_GROUP_ID}","region":"${POWERVS_REGION}","vpcRegion":"","zone":"${POWERVS_ZONE}","serviceInstanceID":"${SERVICE_INSTANCE_GUID}"}}
 ___EOF___
 
 			set +e
