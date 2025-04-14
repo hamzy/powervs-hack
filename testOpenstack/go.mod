7cd83447e922b6dc726f71d3cd291537eb039e67
Mark Hamzy <hamzy@us.ibm.com>
2025-04-14 10:04:01 -0500

    Add testOpenstack

---
+++ testOpenstack/go.mod
@@ -0,0 +1,20 @@
+module example/user/testOpenstack
+
+go 1.24.2
+
+require (
+	github.com/gophercloud/gophercloud/v2 v2.7.0
+	github.com/gophercloud/utils/v2 v2.0.0-20250404104721-4c0f6d93d3a9
+	github.com/h2non/filetype v1.1.1
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.9.3
+	github.com/thedevsaddam/retry v1.2.1
+	github.com/ulikunitz/xz v0.5.12
+	golang.org/x/sys v0.32.0
+)
+
+require (
+	github.com/gofrs/uuid/v5 v5.3.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ testOpenstack/testOpenstack.go
@@ -0,0 +1,566 @@
+//
+// Parts borrowed from:
+//	https://github.com/openshift/installer/tree/main/pkg/infrastructure/openstack/preprovision/rhcosimage.go
+//	https://github.com/openshift/installer/tree/main/pkg/rhcos/cache/cache.go
+//	https://github.com/openshift/installer/tree/main/pkg/types/openstack/defaults/client.go 
+//	https://github.com/openshift/installer/tree/main/pkg/types/openstack/defaults/clientopts.go 
+//
+// (/bin/rm go.*; go mod init example/user/testOpenstack; go mod tidy; go get github.com/h2non/filetype@v1.1.1)
+// (echo "vet:"; go vet || exit 1; echo "build:"; go build *.go || exit 1; ./testOpenstack image-upload --cloud "powervc" --rhcosImage "https://rhcos.mirror.openshift.com/art/storage/prod/streams/rhel-9.6/builds/9.6.20250402-0/ppc64le/rhcos-9.6.20250402-0-powervs.ppc64le.ova.gz" --imageName "hamzy-test" --infraID "rdr-hamzy-openstack-abcde")
+// (echo "vet:"; go vet || exit 1; echo "build:"; go build *.go || exit 1; ./testOpenstack volume-create --cloud "powervc" --volumeName "hamzy-volume-test" --volumeSize 10 --availabilityZone "nova" --imageID "7bf9857f-f4d0-4970-8e11-5310da46d539")
+//
+
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"context"
+	"crypto/sha256"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/gophercloud/gophercloud/v2"
+	"github.com/gophercloud/gophercloud/v2/openstack/image/v2/imagedata"
+	"github.com/gophercloud/gophercloud/v2/openstack/image/v2/images"
+	"github.com/gophercloud/gophercloud/v2/openstack/blockstorage/v2/volumes"
+	"github.com/gophercloud/utils/v2/openstack/clientconfig"
+	"github.com/h2non/filetype/matchers"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/thedevsaddam/retry"
+	"github.com/ulikunitz/xz"
+	"golang.org/x/sys/unix"
+)
+
+// urlWithIntegrity pairs a URL with an optional expected sha256 checksum (after decompression, if any)
+// If the query string contains sha256 parameter (i.e. https://example.com/data.bin?sha256=098a5a...),
+// then the downloaded data checksum will be compared with the provided value.
+type urlWithIntegrity struct {
+	location           url.URL
+	uncompressedSHA256 string
+}
+
+func (u *urlWithIntegrity) uncompressedName() string {
+	n := filepath.Base(u.location.Path)
+	return strings.TrimSuffix(strings.TrimSuffix(n, ".gz"), ".xz")
+}
+
+// download obtains a file from a given URL, puts it in the cache folder, defined by dataType parameter,
+// and returns the local file path.
+func (u *urlWithIntegrity) download(dataType, applicationName string) (string, error) {
+	fileName := u.uncompressedName()
+
+	cacheDir, err := GetCacheDir(dataType, applicationName)
+	if err != nil {
+		return "", err
+	}
+
+	filePath, err := GetFileFromCache(fileName, cacheDir)
+	if err != nil {
+		return "", err
+	}
+	if filePath != "" {
+		// Found cached file
+		return filePath, nil
+	}
+
+	// Send a request to get the file
+	err = retry.DoFunc(3, 5*time.Second, func() error {
+		resp, err := http.Get(u.location.String())
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		// Let's find the content length for future debugging
+		logrus.Debugf("image download content length: %d", resp.ContentLength)
+
+		// Check server response
+		if resp.StatusCode != http.StatusOK {
+			return errors.Errorf("bad status: %s", resp.Status)
+		}
+
+		filePath = filepath.Join(cacheDir, fileName)
+		return cacheFile(resp.Body, filePath, u.uncompressedSHA256)
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return filePath, nil
+}
+
+// GetFileFromCache returns path of the cached file if found, otherwise returns an empty string
+// or error.
+func GetFileFromCache(fileName string, cacheDir string) (string, error) {
+	filePath := filepath.Join(cacheDir, fileName)
+
+	// If the file has already been cached, return its path
+	_, err := os.Stat(filePath)
+	if err == nil {
+		logrus.Debugf("The file was found in cache: %v. Reusing...", filePath)
+		return filePath, nil
+	}
+	if !os.IsNotExist(err) {
+		return "", err
+	}
+
+	return "", nil
+}
+
+// GetCacheDir returns a local path of the cache, where the installer should put the data:
+// <user_cache_dir>/agent/<dataType>_cache
+// If the directory doesn't exist, it will be automatically created.
+func GetCacheDir(dataType, applicationName string) (string, error) {
+	if dataType == "" {
+		return "", errors.Errorf("data type can't be an empty string")
+	}
+
+	userCacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return "", err
+	}
+
+	cacheDir := filepath.Join(userCacheDir, applicationName, dataType+"_cache")
+
+	_, err = os.Stat(cacheDir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.MkdirAll(cacheDir, 0755)
+			if err != nil {
+				return "", err
+			}
+		} else {
+			return "", err
+		}
+	}
+
+	return cacheDir, nil
+}
+
+// cacheFile puts data in the cache.
+func cacheFile(reader io.Reader, filePath string, sha256Checksum string) (err error) {
+	logrus.Debugf("Unpacking file into %q...", filePath)
+
+	flockPath := fmt.Sprintf("%s.lock", filePath)
+	flock, err := os.Create(flockPath)
+	if err != nil {
+		return err
+	}
+	defer flock.Close()
+	defer func() {
+		err2 := os.Remove(flockPath)
+		if err == nil {
+			err = err2
+		}
+	}()
+
+	err = unix.Flock(int(flock.Fd()), unix.LOCK_EX)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err2 := unix.Flock(int(flock.Fd()), unix.LOCK_UN)
+		if err == nil {
+			err = err2
+		}
+	}()
+
+	_, err = os.Stat(filePath)
+	if err != nil && !os.IsNotExist(err) {
+		return nil // another cacheFile beat us to it
+	}
+
+	tempPath := fmt.Sprintf("%s.tmp", filePath)
+
+	// Delete the temporary file that may have been left over from previous launches.
+	err = os.Remove(tempPath)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return errors.Errorf("failed to clean up %s: %v", tempPath, err)
+		}
+	} else {
+		logrus.Debugf("Temporary file %v that remained after the previous launches was deleted", tempPath)
+	}
+
+	file, err := os.OpenFile(tempPath, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0444)
+	if err != nil {
+		return err
+	}
+	closed := false
+	defer func() {
+		if !closed {
+			file.Close()
+		}
+	}()
+
+	// Detect whether we know how to decompress the file
+	// See http://golang.org/pkg/net/http/#DetectContentType for why we use 512
+	buf := make([]byte, 512)
+	_, err = reader.Read(buf)
+	if err != nil {
+		return err
+	}
+
+	reader = io.MultiReader(bytes.NewReader(buf), reader)
+	switch {
+	case matchers.Gz(buf):
+		logrus.Debug("decompressing the image archive as gz")
+		uncompressor, err := gzip.NewReader(reader)
+		if err != nil {
+			return err
+		}
+		defer uncompressor.Close()
+		reader = uncompressor
+	case matchers.Xz(buf):
+		logrus.Debug("decompressing the image archive as xz")
+		uncompressor, err := xz.NewReader(reader)
+		if err != nil {
+			return err
+		}
+		reader = uncompressor
+	default:
+		// No need for an interposer otherwise
+		logrus.Debug("no known archive format detected for image, assuming no decompression necessary")
+	}
+
+	// Wrap the reader in TeeReader to calculate sha256 checksum on the fly
+	hasher := sha256.New()
+	if sha256Checksum != "" {
+		reader = io.TeeReader(reader, hasher)
+	}
+
+	written, err := io.Copy(file, reader)
+	if err != nil {
+		return err
+	}
+
+	// Let's find out how much data was written
+	// for future troubleshooting
+	logrus.Debugf("writing the RHCOS image was %d bytes", written)
+
+	err = file.Close()
+	if err != nil {
+		return err
+	}
+	closed = true
+
+	// Validate sha256 checksum
+	if sha256Checksum != "" {
+		foundChecksum := fmt.Sprintf("%x", hasher.Sum(nil))
+		if sha256Checksum != foundChecksum {
+			logrus.Error("File sha256 checksum is invalid.")
+			return errors.Errorf("Checksum mismatch for %s; expected=%s found=%s", filePath, sha256Checksum, foundChecksum)
+		}
+
+		logrus.Debug("Checksum validation is complete...")
+	}
+
+	return os.Rename(tempPath, filePath)
+}
+
+// DownloadImageFile is a helper function that obtains an image file from a given URL,
+// puts it in the cache and returns the local file path.  If the file is compressed
+// by a known compressor, the file is uncompressed prior to being returned.
+func DownloadImageFile(baseURL string, applicationName string) (string, error) {
+	return DownloadImageFileWithSha(baseURL, applicationName, "")
+}
+
+// DownloadImageFileWithSha sets the sha256Checksum which is checked on download.
+func DownloadImageFileWithSha(baseURL string, applicationName string, sha256Checksum string) (string, error) {
+	logrus.Debugf("Obtaining RHCOS image file from '%v'", baseURL)
+
+	var u urlWithIntegrity
+	parsedURL, err := url.ParseRequestURI(baseURL)
+	if err != nil {
+		return "", err
+	}
+	q := parsedURL.Query()
+	if sha256Checksum != "" {
+		u.uncompressedSHA256 = sha256Checksum
+	}
+	if uncompressedSHA256, ok := q["sha256"]; ok {
+		if sha256Checksum != "" && uncompressedSHA256[0] != sha256Checksum {
+			return "", errors.Errorf("supplied sha256Checksum does not match URL")
+		}
+		u.uncompressedSHA256 = uncompressedSHA256[0]
+		q.Del("sha256")
+		parsedURL.RawQuery = q.Encode()
+	}
+	u.location = *parsedURL
+
+	return u.download("image", applicationName)
+}
+
+// UploadBaseImage creates a new image in Glance and uploads the RHCOS image there.
+func UploadBaseImage(ctx context.Context, cloud string, rhcosImage string, imageName string, infraID string, imageProperties map[string]string) error {
+	url, err := url.Parse(rhcosImage)
+	if err != nil {
+		return err
+	}
+
+	// We support 'http(s)' and 'file' schemes. If the scheme is http(s), then we will upload a file from that
+	// location. Otherwise will take local file path from the URL.
+	var localFilePath string
+	switch url.Scheme {
+	case "http", "https":
+		localFilePath, err = DownloadImageFile(rhcosImage, "openshift-installer")
+		if err != nil {
+			return err
+		}
+	case "file":
+		localFilePath = filepath.FromSlash(url.Path)
+	default:
+		return fmt.Errorf("unsupported URL scheme: %q", url.Scheme)
+	}
+
+	logrus.Debugln("Creating a Glance image for RHCOS...")
+
+	f, err := os.Open(localFilePath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	conn, err := NewServiceClient(ctx, "image", DefaultClientOpts(cloud))
+	if err != nil {
+		return err
+	}
+
+	// By default we use "qcow2" disk format, but if the file extension is "raw",
+	// then we set the disk format as "raw".
+	diskFormat := "qcow2"
+	if extension := filepath.Ext(localFilePath); extension == "raw" {
+		diskFormat = "raw"
+	}
+
+	img, err := images.Create(ctx, conn, images.CreateOpts{
+		Name:            imageName,
+		ContainerFormat: "bare",
+		DiskFormat:      diskFormat,
+		Tags:            []string{"openshiftClusterID=" + infraID},
+		Properties:      imageProperties,
+	}).Extract()
+	if err != nil {
+		return err
+	}
+
+	// Use direct upload (see
+	// https://github.com/openshift/installer/issues/3403 for a discussion
+	// on web-download)
+	logrus.Debugf("Upload RHCOS to the image %q (%s)", img.Name, img.ID)
+	res := imagedata.Upload(ctx, conn, img.ID, f)
+	if res.Err != nil {
+		return err
+	}
+	logrus.Debugf("RHCOS image upload completed.")
+
+	return nil
+}
+
+// getUserAgent generates a Gophercloud UserAgent to help cloud operators
+// disambiguate openshift-installer requests.
+func getUserAgent() (gophercloud.UserAgent, error) {
+	ua := gophercloud.UserAgent{}
+
+	ua.Prepend(fmt.Sprintf("openshift-installer/%s", "1.0"))
+	return ua, nil
+}
+
+// NewServiceClient is a wrapper around Gophercloud's NewServiceClient that
+// ensures we consistently set a user-agent.
+func NewServiceClient(ctx context.Context, service string, opts *clientconfig.ClientOpts) (*gophercloud.ServiceClient, error) {
+	ua, err := getUserAgent()
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := clientconfig.NewServiceClient(ctx, service, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	client.UserAgent = ua
+
+	return client, nil
+}
+
+// DefaultClientOpts generates default client opts based on cloud name
+func DefaultClientOpts(cloudName string) *clientconfig.ClientOpts {
+	opts := new(clientconfig.ClientOpts)
+	opts.Cloud = cloudName
+	// We explicitly disable reading auth data from env variables by setting an invalid EnvPrefix.
+	// By doing this, we make sure that the data from clouds.yaml is enough to authenticate.
+	// For more information: https://github.com/gophercloud/utils/blob/8677e053dcf1f05d0fa0a616094aace04690eb94/openstack/clientconfig/requests.go#L508
+	opts.EnvPrefix = "NO_ENV_VARIABLES_"
+	return opts
+}
+
+func createVolume (ctx context.Context, cloud string, volumeName string, volumeSize int, availabilityZone string, imageID string) error {
+	var (
+		err error
+	)
+
+	conn, err := NewServiceClient(ctx, "volume", DefaultClientOpts(cloud))
+	if err != nil {
+		panic(err)
+	}
+
+//	schedulerHintOpts := volumes.SchedulerHintOpts{
+//		SameHost: []string{
+//			"e980",
+//		},
+//	}
+
+	createOpts := volumes.CreateOpts{
+		Name:              volumeName,
+		Size:              volumeSize,
+		AvailabilityZone:  availabilityZone,
+		ImageID:           imageID,
+	}
+
+	volume, err := volumes.Create(ctx, conn, createOpts, nil).Extract()
+	if err != nil {
+		panic(err)
+	}
+
+	logrus.Debugf("volume = %+v", volume)
+
+	return nil
+}
+
+func imageUploadCommand (imageUploadFlags *flag.FlagSet, args []string) error {
+
+	var (
+		ptrCloud            *string
+		ptrRhcosImage       *string
+		ptrImageName        *string
+		ptrInfraID          *string
+
+		ctx                 context.Context
+		cancel              context.CancelFunc
+
+		imageProperties     map[string]string
+	)
+
+	ptrCloud = imageUploadFlags.String("cloud", "", "The cloud to use in clouds.yaml")
+	ptrRhcosImage = imageUploadFlags.String("rhcosImage", "", "The URL to download the RHCOS image")
+	ptrImageName = imageUploadFlags.String("imageName", "", "The image name to save into")
+	ptrInfraID = imageUploadFlags.String("infraID", "", "The infrastructure ID to tag with")
+
+	imageUploadFlags.Parse(args)
+
+	if ptrCloud == nil || *ptrCloud == "" {
+		fmt.Println("Error: --cloud not specified")
+		os.Exit(1)
+	}
+	if ptrRhcosImage == nil || *ptrRhcosImage == "" {
+		fmt.Println("Error: --rhcosImage not specified")
+		os.Exit(1)
+	}
+	if ptrImageName == nil || *ptrImageName == "" {
+		fmt.Println("Error: --imageName not specified")
+		os.Exit(1)
+	}
+	if ptrInfraID == nil || *ptrInfraID == "" {
+		fmt.Println("Error: --infraID not specified")
+		os.Exit(1)
+	}
+	if len(imageUploadFlags.Args()) != 0 {
+		fmt.Printf("Error: extra options specified: %v\n", imageUploadFlags.Args())
+		os.Exit(1)
+	}
+
+	ctx, cancel = context.WithTimeout(context.TODO(), 15*time.Minute)
+	defer cancel()
+
+	return UploadBaseImage(ctx, *ptrCloud, *ptrRhcosImage, *ptrImageName, *ptrInfraID, imageProperties)
+}
+
+func volumeCreateCommand (volumeCreateFlags *flag.FlagSet, args []string) error {
+
+	var (
+		ptrCloud            *string
+		ptrVolumeName       *string
+		ptrVolumeSize       *int
+		ptrAvailabilityZone *string
+		ptrImageID          *string
+
+		ctx                 context.Context
+		cancel              context.CancelFunc
+	)
+
+	ptrCloud = volumeCreateFlags.String("cloud", "", "The cloud to use in clouds.yaml")
+	ptrVolumeName = volumeCreateFlags.String("volumeName", "", "The name of the volume to create")
+	ptrVolumeSize = volumeCreateFlags.Int("volumeSize", 0, "The size of the volume to create")
+	ptrAvailabilityZone = volumeCreateFlags.String("availabilityZone", "", "The availability zone to use")
+	ptrImageID = volumeCreateFlags.String("imageID", "", "The UUID of the image to copy from")
+
+	volumeCreateFlags.Parse(args)
+
+	if ptrCloud == nil || *ptrCloud == "" {
+		fmt.Println("Error: --cloud not specified")
+		os.Exit(1)
+	}
+	if ptrVolumeName == nil || *ptrVolumeName == "" {
+		fmt.Println("Error: --volumeName not specified")
+		os.Exit(1)
+	}
+	if ptrVolumeSize == nil {
+		fmt.Println("Error: --volumeSize not specified")
+		os.Exit(1)
+	}
+	if *ptrVolumeSize == 0 {
+		fmt.Println("Error: --volumeSize should not be 0")
+		os.Exit(1)
+	}
+	if ptrAvailabilityZone == nil || *ptrAvailabilityZone == "" {
+		fmt.Println("Error: --availabilityZone not specified")
+		os.Exit(1)
+	}
+	if ptrImageID == nil || *ptrImageID == "" {
+		fmt.Println("Error: --imageID not specified")
+		os.Exit(1)
+	}
+
+	ctx, cancel = context.WithTimeout(context.TODO(), 15*time.Minute)
+	defer cancel()
+
+	return createVolume (ctx, *ptrCloud, *ptrVolumeName, *ptrVolumeSize, *ptrAvailabilityZone, *ptrImageID)
+}
+
+func main () {
+
+	var (
+		imageUploadFlags  *flag.FlagSet
+		volumeCreateFlags *flag.FlagSet
+		err               error
+	)
+
+	imageUploadFlags = flag.NewFlagSet("image-upload", flag.ExitOnError)
+	volumeCreateFlags = flag.NewFlagSet("volume-create", flag.ExitOnError)
+
+	switch strings.ToLower(os.Args[1]) {
+	case "image-upload":
+		err = imageUploadCommand(imageUploadFlags, os.Args[2:])
+
+	case "volume-create":
+		err = volumeCreateCommand(volumeCreateFlags, os.Args[2:])
+
+	default:
+		fmt.Printf("Error: Unknown command %s\n", os.Args[1])
+		os.Exit(1)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+}
